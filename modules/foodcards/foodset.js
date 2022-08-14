d915eea36924dd2cf256859e32f8b3ac7287666f
Daniel Rodrigues <dannepi.dev@gmail.com>
2022-08-14 07:49:35 -0300

    Cards terminados e configurados

--- modules/foodcards/foodset.js
+++ modules/foodcards/foodset.js
@@ -1,17 +1,36 @@
 import { foodlistcontent } from "./content.js";
 
-export function FoodSetter(comida, num){
+export function FoodSetter(comida, thisId){
         
-    const typeSelected = 'foodlistcontent.'+comida+'['+num+'].'
+    const typeSelected = 'foodlistcontent.'+comida+'['+thisId+'].'
+    let specialchar;
+    let myIcon;
+    switch (comida) {
+        case "comida":
+            specialchar = "c";
+            myIcon = "pizza"
+        break;
+        case "bebida":
+            specialchar = "b";
+            myIcon = "beer"
+            break;
+        case "sobremesa":
+            specialchar = "s";
+            myIcon = "ice-cream"
+            break;
+    }
 
     return /*html*/`
-<div id="box-card">
-        <img src="${eval(typeSelected+'img')}" alt="">
-        <span class="title">${eval(typeSelected+'title')}</span>
-        <span class="sub">${eval(typeSelected+'sub')}</span>
-    <div class="price">
-        <h3>${eval(typeSelected+'price')}</h3>
+<button class="${comida}" id="${specialchar}b${thisId}" onclick="menuComidas('${comida}', '${specialchar}b${thisId}', '${specialchar}i${thisId}')">
+    <div id="box-card">
+            <img src="${eval(typeSelected+'img')}" alt="">
+            <span class="title">${eval(typeSelected+'title')}</span>
+            <span class="sub">${eval(typeSelected+'sub')}</span>
+        <div class="price">
+            <h3>${eval(typeSelected+'price')}</h3>
+        </div>
     </div>
-</div>
+    <ion-icon id="${specialchar}i${thisId}" name="${myIcon}"></ion-icon>
+</button>
 `;
 }
