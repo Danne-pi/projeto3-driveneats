7e24eda4188822e3cc0bdfc920ed87404311f611
Daniel Rodrigues <dannepi.dev@gmail.com>
2022-08-15 09:06:23 -0300

    Finalizado.

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import { firstAlert } from "./modules/firstalert/firstalert.js";
 import { setOrder } from "./modules/buttons/orderbutton.js"
 import { basicLoad } from "./modules/pages/basic.js";
 
